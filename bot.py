f94ae390c899f3ab7d994bd8fb7f154a06c13ef6
esh-bc <singhyashraj432.singhyashraj.3@gmail.com>
2026-08-26 17:19:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8', errors='repla
 BOT_TOKEN = "8756139201:AAEYlUyNPvNQaNoK0YyTD0HYMXe1WZAf-FE"
 OWNER_ID = 8189708860
 CHANNEL_LINK = "https://t.me/+PrJMSB4bG5o2OTk1"
-CHANNEL_ID = 
+CHANNEL_ID = -1003808606039
 MAX_RETRIES = 3
 MAX_RETRIES_ADMIN = 8
 POST_AUTH_RETRIES = 3
